c5bfe6a3c53c5e6af6d7cfde76c3fc983ccfbe8f
SamarV-121 <samar@samarv121.dev>
2025-03-19 08:41:32 +0530

    tanzanite: Update vibrator effects config - LineageOS/android_hardware_mediatek@203f218
    Change-Id: I7370f77b073308dec87848771ee8b4eb386e38f5
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,6 +181,10 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+
+# Vibrator
+$(call soong_config_set,mediatek_vibrator,supports_effects,true)
+
 # Wi-Fi
 BOARD_WLAN_DEVICE := MediaTek
 WPA_SUPPLICANT_VERSION := VER_0_8_X

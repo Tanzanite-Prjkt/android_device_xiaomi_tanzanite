4af207205c7746f3f8727b272b73cc76a601a421
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    tanzanite: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59

--- device.mk
+++ device.mk
@@ -76,6 +76,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.default_recovery
 
 # Audio
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 TARGET_EXCLUDES_AUDIOFX := true
 
 $(call soong_config_set,android_hardware_audio,run_64bit,true)

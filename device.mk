1fda46735eaa6ab9b57aa0750bdb217eeb61040d
Shirayuki39 <lorddemecrius83@proton.me>
2026-04-08 05:26:09 +0800

    tanzanite: Bump opengl, vulkan deqp level to 2024-03-01
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Change-Id: I6ae3f025842e308a5e97ad5c905501ddd02f9fa0
    Signed-off-by: Nathan <nathannxx@proton.me>

--- device.mk
+++ device.mk
@@ -131,8 +131,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek \
-    android.software.vulkan.deqp.level-2021-03-01.prebuilt.xml \
-    android.software.opengles.deqp.level-2021-03-01.prebuilt.xml
+    android.software.vulkan.deqp.level-2024-03-01.prebuilt.xml \
+    android.software.opengles.deqp.level-2024-03-01.prebuilt.xml
 
 # Dolby
 $(call inherit-product-if-exists, hardware/dolby/dolby.mk)

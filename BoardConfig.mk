2defe8e69cd622bf4b89641b1826413bfe724966
itswill00 <anstykx00@gmail.com>
2026-05-08 11:43:08 +0700

    tanzanite: BoardConfig: Optimize kernel command line
    Add transparent_hugepage=madvise to the kernel command line.
    This allows applications to use hugepages explicitly while preventing
    unnecessary memory overhead for the rest of the system.
    
    Signed-off-by: itswill00 <anstykx00@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ BOARD_BOOTCONFIG := androidboot.serialconsole=0
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 BOARD_KERNEL_CMDLINE += rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy=1
+BOARD_KERNEL_CMDLINE += transparent_hugepage=madvise
 
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x3fff8000
